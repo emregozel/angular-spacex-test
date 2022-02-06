5a25b434bd41fe38d345aa0af21da988a97c44cb
Emre Gozel <emre@gozel.org>
2022-02-06 22:45:34 +0300

    improvment code quailty

--- src/app/launch-list/launch-list.component.spec.ts
+++ src/app/launch-list/launch-list.component.spec.ts
@@ -1,12 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule, MatProgressSpinnerModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
 import { ApolloModule } from 'apollo-angular';
 import { DateAgoPipe } from '../pipes/date-ago.pipe';
 import { LaunchFacadeService } from '../services/launch-facade.service';
-import { PastLaunchesListGQL } from '../services/spacexGraphql.service';
 import { launchReducers } from '../store';
 
 import { LaunchListComponent } from './launch-list.component';
